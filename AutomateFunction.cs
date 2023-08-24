b87443b717b249574f0f5d2f7fa5e8db963503cb
Matteo Cominetti <matteo@speckle.systems>
2023-08-24 15:18:28 +0100

    fix

--- AutomateFunction.cs
+++ AutomateFunction.cs
@@ -5,18 +5,6 @@ using Speckle.Core.Models;
 using Speckle.Core.Models.Extensions;
 using Speckle.Core.Transports;
 using System.ComponentModel.DataAnnotations;
-
-/// <summary>
-/// This class describes the user specified variables that the function wants to work with.
-/// </summary>
-/// This class is used to generate a JSON Schema to ensure that the user provided values
-/// are valid and match the required schema.
-class FunctionInputs
-{
-  [Required]
-  public string SpeckleTypeToCount;
-}
-
 class AutomateFunction
 {
   public static async Task<string> Run(
@@ -31,10 +19,11 @@ class AutomateFunction
     };
     var client = new Client(account);
 
+
     var streamId = await client.StreamCreate(new StreamCreateInput { description = "Speckle Automate is Awesome ⭐", name = "Automated Stream" });
     var data = new Base();
     data["matteos-prop"] = "zis iz a test";
-    var commitId = await Helpers.Send(streamId, data, "I'M A BOT", "automate");
+    var commitId = await Helpers.Send(speckleProjectData.ProjectId, data, "I'M A BOT", "automate");
 
 
     return $"{speckleProjectData.SpeckleServerUrl}/streams/{streamId}/commits/{commitId}";
